346d82bf4c9732ad44378dba4bf5a50a775822ce
EmirVelazquez <emirleovelazquez@gmail.com>
2020-05-01 22:38:49 -0500

    changed sidenav to left of viewport. added freecodecamp testing cdn, added required sections for test passing. added some sass styling.

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("App.js working.")
 
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, { edge: "right", draggable: true });
+    var instances = M.Sidenav.init(elems, { edge: "left", draggable: true });
 
 });
