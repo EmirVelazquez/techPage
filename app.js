cd8cd0cf4710ecc1efeef14552b369c67c4a69f3
EmirVelazquez <emirleovelazquez@gmail.com>
2020-05-06 12:45:18 -0500

    added png asset. added logic to handle display of fcc test suit button. modified index.html. added sass styling.

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("App.js working.")
+    // ==================================================
+    // References to elements or Global variables
+    // ==================================================
 
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, { edge: "left", draggable: true });
+    const fccButton = document.getElementById("fcc_test_suite_wrapper");
+    const fccButtonToggle = document.getElementById("scriptTest");
+    var sidenav = document.querySelectorAll('.sidenav');
+    var tooltip = document.querySelectorAll('.tooltipped');
+    // Flag for toggle
+    var showing = false;
+    // Settings for materialize components & initialization
+    var instancesSideNav = M.Sidenav.init(sidenav, { edge: "left", draggable: true });
+    var instancesTooltip = M.Tooltip.init(tooltip);
 
+    // ==================================================
+    // Main Executions
+    // ==================================================
+
+    toggleTest();
+
+    // ==================================================
+    // Helper Functions
+    // ==================================================
+
+    // This function handles the toggle for the test script from freeCodeCamp
+    function toggleTest() {
+        fccButtonToggle.addEventListener("click", function () {
+            if (showing) {
+                fccButton.style.display = "none";
+                showing = false;
+            } else {
+                fccButton.style.display = "block";
+                showing = true;
+            }
+        });
+    };
 });
